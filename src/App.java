a76b70ad567ffd3aa898b9494cd160eec2e28f93
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2023-08-07 23:44:14 +0100

    Standard login code.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    
+        Parent root;
+
+try {
+    
+    /* Loads the initial first page that's built using the FXML scenebuilder. */
+
+    root = FXMLLoader.load(getClass().getResource("FXML/login.fxml"));
+    Scene scene = new Scene(root);
+    scene.getStylesheets().add("FXML/login.css");
+    primaryStage.setTitle("Spartan Fitness.");
+    primaryStage.setScene(scene);
+    primaryStage.show();} catch (IOException e) {
+
+    e.printStackTrace();
+}
+}
+ 
+     public static void main(String[] args) {
+        launch(args);
     }
 }
